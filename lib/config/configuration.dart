ababd0ba76e801de4c74a5f3aa4b868de68b5738
Ricky Legada <rickylegada@gmail.com>
2025-01-29 21:14:31 +0800

    feat: add default configuration file

--- lib/config/configuration.dart
+++ lib/config/configuration.dart
@@ -1,3 +1,7 @@
-String getApplicationName() {
-  return "f";
+class Config {
+  static const String appName = "";
+  static const String apiEndPoint = "";
+  static const double defaultPadding = 16.0;
+  static const double defaultMargin = 16.0;
+
 }
